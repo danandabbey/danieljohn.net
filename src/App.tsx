b86c43176926d7e319a150d1a0064fd0731598e3
danandabbey <danandabbey502@gmail.com>
2023-10-19 09:35:46 -0400

    added photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
+import style from './assets/styles'
 
 const App = () => {
-
-  const app = {
-    'alignItems': 'center',
-    'height': '100vh',
-    'width': '100vw',
-  }
-
-  const textStyle: any = {
-    'color': '#fff',
-    'fontSize': '32px',
-  }
-  const container = {
-    'display': 'flex',
-    'alignItems': 'center',
-    'backgroundColor': '#000',
-    'justifyContent': 'center',
-    'height': '20%',
-    'width': '100%'
-  }
-    
   return (
-    <div style={app}>
-      <div style={container}>
-        <h1 style={textStyle}>We are currently under construction</h1>
-      </div>
+    <div style={style.app}>
     </div>
   )
 };
---
+++ src/assets/styles.ts
@@ -0,0 +1,13 @@
+const style = {
+    'app': {
+        'backgroundImage' : 'url(./portrait.jpg)',
+        'backgroundRepeat': 'no-repeat',
+        'backgroundSize': 'cover',
+        'backgroundAttachment': 'fixed',
+        'backgroundPosition': 'center center',
+        'width': '100vw',
+        'height' : '100vh',
+    }
+};
+    
+export default style
